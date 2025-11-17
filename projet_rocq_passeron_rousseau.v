fe655ef5893eaccf9cb03ff89252f1b80448d900
Paul-Passeron <paul.passeron2@gmail.com>
2025-11-17 15:39:51 +0100

    Proved InMultiset_removeOne_still_in

--- projet_rocq_passeron_rousseau.v
+++ projet_rocq_passeron_rousseau.v
@@ -1031,7 +1031,44 @@ Lemma InMultiset_removeOne_still_in:
   forall x s, wf s -> multiplicity x s > 1 ->
   InMultiset x (removeOne x s).
 Proof.
-Admitted.
+  intros x s Hwfs Hmul.
+  induction s.
+  simpl in *.
+  lia.
+  destruct a as [a an].
+  simpl.
+  destruct (T_eq_dec a x) as [Hax | Hax].
+  - subst x.
+    destruct (an == 1) as [Han | Han].
+    + rewrite Han.
+      simpl.
+      assert (an = 1).
+      assumption.
+      subst an.
+      simpl in Hmul.
+      destruct (T_eq_dec a a).
+      * lia.
+      * contradiction.
+    + assert (Hd: an <> 1).
+      assumption.
+      assert(H' := Hd).
+      apply Nat.eqb_neq in H'.
+      rewrite H'.
+      unfold InMultiset.
+      simpl.
+      destruct (T_eq_dec a a).
+      * reflexivity.
+      * contradiction.
+  - unfold InMultiset.
+    simpl.
+    simpl in Hmul.
+    destruct (T_eq_dec a x).
+    + reflexivity.
+    + destruct Hwfs as [_ [_ Hwfs]].
+      assert (H := IHs Hwfs Hmul).
+      unfold InMultiset in H.
+      exact H.
+Qed.
 
 (* Pas InMultiset après removeOne si multiplicité = 1 *)
 Lemma not_InMultiset_removeOne_gone:
